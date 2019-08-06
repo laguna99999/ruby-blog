24fc730b6e047abbe3d4428a79e169b4f65ae3cc
laguna99999 <lookatme99999@yahoo.com>
2019-08-06 20:06:58 +1000

    Article list view and single view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+    def index
+        @articles = Article.all
+    end
+
     def new
     end
 
@@ -8,6 +12,11 @@ class ArticlesController < ApplicationController
         @article.save
         redirect_to @article
     end
+
+    def show
+        @article = Article.find(params[:id])
+    end
+
     private
         def article_params
             params.require(:article).permit(:title, :text)
